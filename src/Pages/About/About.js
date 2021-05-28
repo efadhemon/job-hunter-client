37c786da32cac50a787f9b40f08c19876040bf29
MousumiMitu <mousumitu@gmail.com>
2021-05-28 21:08:45 +0600

    new job posting section

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import './About.css';
+import React from "react";
+import "./About.css";
 
 const About = () => {
-
-    return (
-
-        <section id="about" className="about section">
-            <div className="container">
-                <div className="row">
-                    <div className="section-title padd-15">
-                        <h2>About Me</h2>
-                    </div>
-                </div>
-                <div className="row">
-                    <h2>This is about</h2>
-                </div>
-            </div>
-        </section>
-
-    );
+  return (
+    <section id="about" className="about section">
+      <div className="container">
+        <div className="row">
+          <div className="section-title padd-15">
+            <h2>About Me</h2>
+          </div>
+        </div>
+        <div className="row">
+          <h2>Form</h2>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
