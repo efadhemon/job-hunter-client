73df84c5b483b9030c8a6ee23b26d357a597c54f
albiummid <albi.ummid@gmail.com>
2021-05-28 20:01:14 +0600

    added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <section id="home" className="home section">
             <div className="container">
-                <h1>I am From Home</h1>
+                <h1>I Have Checked</h1>
             </div>
         </section>
     );
