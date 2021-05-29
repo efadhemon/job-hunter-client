89f205d9d544680c7c829006a542e7611fe75109
MousumiMitu <mousumitu@gmail.com>
2021-05-29 22:12:57 +0600

    form changes

--- src/Pages/JobPosting/JobPosting.js
+++ src/Pages/JobPosting/JobPosting.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import "./JobPosting.css";
+import axios from "axios";
 
 const JobPosting = () => {
+  const [imgData, setImgData] = useState(null);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => console.log(data);
+
+  const handleImageUpload = (e) => {
+    const logoData = new FormData();
+    logoData.set("key", "bb996e7457cab7b5b10c725d84190723");
+    logoData.append("image", e.target.files[0]);
+
+    axios
+      .post("https://api.imgbb.com/1/upload", logoData)
+      .then(function (response) {
+        setImgData(response.data.data.display_url);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
   return (
     <section id="about" className="about section">
       <div className="my-container">
@@ -25,6 +42,11 @@ const JobPosting = () => {
                 <input {...register("company", { required: true })} />
                 {errors.company && <p>company name is required.</p>}
               </div>
+              <div className="input-area">
+                <label>Experience *</label> <br />
+                <input {...register("experience", { required: true })} />
+                {errors.experience && <p>Experience is required.</p>}
+              </div>
 
               <div className="input-area">
                 <label>Description</label>
@@ -33,6 +55,10 @@ const JobPosting = () => {
                   {...register("description", { required: true })}
                 ></textarea>
                 {errors.description && <p>description is required</p>}
+              </div>
+              <div className="input-area">
+                <label>Company logo</label>
+                <input type="file" onChange={handleImageUpload} />
               </div>
               <div className="input-area">
                 <select
