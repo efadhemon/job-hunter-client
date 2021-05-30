4fdce24618b3a975550bbb60e17ae472c81b92da
albiummid <albi.ummid@gmail.com>
2021-05-30 13:14:24 +0600

    home page completed

---
+++ src/FakeData/JobsData.js
@@ -0,0 +1,269 @@
+import JavaScriptIcon from '../images/tech-javascript.svg'
+import reactJsIcon from '../images/tech-reactjs.svg'
+import dataIcon from '../images/tech-data-analysis.svg'
+import django from '../images/tech-django.svg'
+import aws from '../images/tech-aws.svg'
+const jobsData = [
+    {
+        role: [
+            { title: 'Writing reusable, testable, and efficient code.' },
+            { title: 'Take an active part in architecture decisions and complex development tasks.' },
+            { title: 'Design and implement low-latency, high-availability, and performant applications.' },
+            { title: 'Motivated to learn new technologies and bring them to life through proper implementations.' },
+            { title: 'Work and communicate in a team environment.' },
+            { title: 'Like to take on responsibilities and think proactively.' },
+        ],
+        icon: JavaScriptIcon,
+        title: 'Js Developer',
+        companyName: 'Programming Hero',
+        employe: 12345,
+        experience: '2-5 Years',
+        jobType: 'Full Time',
+        salary: '45k-50k',
+        about: 'We are looking for a Node.js Developer responsible for managing the interchange of data between the server and the users. Your primary focus will be the development of all server-side logic, definition and maintenance of the central database, and ensuring high performance and responsiveness to requests from the front-end. You will also be responsible for integrating the front-end elements built by your co-workers into the application. Therefore, a basic understanding of front-end technologies is necessary as well. ',
+        skills: [
+            { title: 'JavaScript' },
+            { title: 'React Js' },
+            { title: 'Node Js' },
+            { title: 'Experss Js' },
+            { title: 'Styled Components' },
+        ]
+
+    },
+    {
+        role: [
+            { title: 'Writing reusable, testable, and efficient code.' },
+            { title: 'Take an active part in architecture decisions and complex development tasks.' },
+            { title: 'Design and implement low-latency, high-availability, and performant applications.' },
+            { title: 'Motivated to learn new technologies and bring them to life through proper implementations.' },
+            { title: 'Work and communicate in a team environment.' },
+            { title: 'Like to take on responsibilities and think proactively.' },
+        ],
+        icon: reactJsIcon,
+        title: 'React Developer',
+        companyName: 'Programming Dev',
+        employe: 12345,
+        experience: '2-5 Years',
+        jobType: 'Part Time',
+        salary: '45k-50k',
+        about: 'We are looking for a Node.js Developer responsible for managing the interchange of data between the server and the users. Your primary focus will be the development of all server-side logic, definition and maintenance of the central database, and ensuring high performance and responsiveness to requests from the front-end. You will also be responsible for integrating the front-end elements built by your co-workers into the application. Therefore, a basic understanding of front-end technologies is necessary as well. ',
+        skills: [
+            { title: 'JavaScript' },
+            { title: 'React Js' },
+            { title: 'Node Js' },
+            { title: 'Experss Js' },
+            { title: 'Styled Components' },
+        ]
+
+    },
+    {
+        icon: dataIcon,
+        title: 'Data Analyser',
+        companyName: ' Dev.to',
+        employe: 12345,
+        experience: '7-10 Years',
+        jobType: 'Full Time',
+        salary: '145k-150k',
+        role: [
+            { title: 'Writing reusable, testable, and efficient code.' },
+            { title: 'Take an active part in architecture decisions and complex development tasks.' },
+            { title: 'Design and implement low-latency, high-availability, and performant applications.' },
+            { title: 'Motivated to learn new technologies and bring them to life through proper implementations.' },
+            { title: 'Work and communicate in a team environment.' },
+            { title: 'Like to take on responsibilities and think proactively.' },
+        ],
+        about: 'We are looking for a Node.js Developer responsible for managing the interchange of data between the server and the users. Your primary focus will be the development of all server-side logic, definition and maintenance of the central database, and ensuring high performance and responsiveness to requests from the front-end. You will also be responsible for integrating the front-end elements built by your co-workers into the application. Therefore, a basic understanding of front-end technologies is necessary as well. ',
+        skills: [
+            { title: 'JavaScript' },
+            { title: 'React Js' },
+            { title: 'Node Js' },
+            { title: 'Experss Js' },
+            { title: 'Styled Components' },
+        ]
+
+    },
+    {
+        role: [
+            { title: 'Writing reusable, testable, and efficient code.' },
+            { title: 'Take an active part in architecture decisions and complex development tasks.' },
+            { title: 'Design and implement low-latency, high-availability, and performant applications.' },
+            { title: 'Motivated to learn new technologies and bring them to life through proper implementations.' },
+            { title: 'Work and communicate in a team environment.' },
+            { title: 'Like to take on responsibilities and think proactively.' },
+        ],
+        icon: django,
+        title: 'django Developer',
+        companyName: 'Programming Dev',
+        employe: 12345,
+        experience: '2-4 Years',
+        jobType: 'Part Time / Full time',
+        salary: '45k-50k',
+        about: 'We are looking for a Node.js Developer responsible for managing the interchange of data between the server and the users. Your primary focus will be the development of all server-side logic, definition and maintenance of the central database, and ensuring high performance and responsiveness to requests from the front-end. You will also be responsible for integrating the front-end elements built by your co-workers into the application. Therefore, a basic understanding of front-end technologies is necessary as well. ',
+        skills: [
+            { title: 'JavaScript' },
+            { title: 'React Js' },
+            { title: 'Node Js' },
+            { title: 'Experss Js' },
+            { title: 'Styled Components' },
+        ]
+
+    },
+    {
+        role: [
+            { title: 'Writing reusable, testable, and efficient code.' },
+            { title: 'Take an active part in architecture decisions and complex development tasks.' },
+            { title: 'Design and implement low-latency, high-availability, and performant applications.' },
+            { title: 'Motivated to learn new technologies and bring them to life through proper implementations.' },
+            { title: 'Work and communicate in a team environment.' },
+            { title: 'Like to take on responsibilities and think proactively.' },
+        ],
+        icon: aws,
+        title: 'AWS Developer',
+        companyName: 'Programming Dev',
+        employe: 12345,
+        experience: '2-5 Years',
+        jobType: 'Part Time',
+        salary: '45k-50k',
+        about: 'We are looking for a Node.js Developer responsible for managing the interchange of data between the server and the users. Your primary focus will be the development of all server-side logic, definition and maintenance of the central database, and ensuring high performance and responsiveness to requests from the front-end. You will also be responsible for integrating the front-end elements built by your co-workers into the application. Therefore, a basic understanding of front-end technologies is necessary as well. ',
+        skills: [
+            { title: 'JavaScript' },
+            { title: 'React Js' },
+            { title: 'Node Js' },
+            { title: 'Experss Js' },
+            { title: 'Styled Components' },
+        ]
+
+    },
+    {
+        role: [
+            { title: 'Writing reusable, testable, and efficient code.' },
+            { title: 'Take an active part in architecture decisions and complex development tasks.' },
+            { title: 'Design and implement low-latency, high-availability, and performant applications.' },
+            { title: 'Motivated to learn new technologies and bring them to life through proper implementations.' },
+            { title: 'Work and communicate in a team environment.' },
+            { title: 'Like to take on responsibilities and think proactively.' },
+        ],
+        icon: reactJsIcon,
+        title: 'React Developer',
+        companyName: 'Programming Dev',
+        employe: 12345,
+        experience: '2-5 Years',
+        jobType: 'Part Time',
+        salary: '45k-50k',
+        about: 'We are looking for a Node.js Developer responsible for managing the interchange of data between the server and the users. Your primary focus will be the development of all server-side logic, definition and maintenance of the central database, and ensuring high performance and responsiveness to requests from the front-end. You will also be responsible for integrating the front-end elements built by your co-workers into the application. Therefore, a basic understanding of front-end technologies is necessary as well. ',
+        skills: [
+            { title: 'JavaScript' },
+            { title: 'React Js' },
+            { title: 'Node Js' },
+            { title: 'Experss Js' },
+            { title: 'Styled Components' },
+        ]
+
+    },
+    {
+        role: [
+            { title: 'Writing reusable, testable, and efficient code.' },
+            { title: 'Take an active part in architecture decisions and complex development tasks.' },
+            { title: 'Design and implement low-latency, high-availability, and performant applications.' },
+            { title: 'Motivated to learn new technologies and bring them to life through proper implementations.' },
+            { title: 'Work and communicate in a team environment.' },
+            { title: 'Like to take on responsibilities and think proactively.' },
+        ],
+        icon: reactJsIcon,
+        title: 'React Developer',
+        companyName: 'Programming Dev',
+        employe: 12345,
+        experience: '2-5 Years',
+        jobType: 'Part Time',
+        salary: '45k-50k',
+        about: 'We are looking for a Node.js Developer responsible for managing the interchange of data between the server and the users. Your primary focus will be the development of all server-side logic, definition and maintenance of the central database, and ensuring high performance and responsiveness to requests from the front-end. You will also be responsible for integrating the front-end elements built by your co-workers into the application. Therefore, a basic understanding of front-end technologies is necessary as well. ',
+        skills: [
+            { title: 'JavaScript' },
+            { title: 'React Js' },
+            { title: 'Node Js' },
+            { title: 'Experss Js' },
+            { title: 'Styled Components' },
+        ]
+
+    },
+    {
+        role: [
+            { title: 'Writing reusable, testable, and efficient code.' },
+            { title: 'Take an active part in architecture decisions and complex development tasks.' },
+            { title: 'Design and implement low-latency, high-availability, and performant applications.' },
+            { title: 'Motivated to learn new technologies and bring them to life through proper implementations.' },
+            { title: 'Work and communicate in a team environment.' },
+            { title: 'Like to take on responsibilities and think proactively.' },
+        ],
+        icon: reactJsIcon,
+        title: 'React Developer',
+        companyName: 'Programming Dev',
+        employe: 12345,
+        experience: '2-5 Years',
+        jobType: 'Part Time',
+        salary: '45k-50k',
+        about: 'We are looking for a Node.js Developer responsible for managing the interchange of data between the server and the users. Your primary focus will be the development of all server-side logic, definition and maintenance of the central database, and ensuring high performance and responsiveness to requests from the front-end. You will also be responsible for integrating the front-end elements built by your co-workers into the application. Therefore, a basic understanding of front-end technologies is necessary as well. ',
+        skills: [
+            { title: 'JavaScript' },
+            { title: 'React Js' },
+            { title: 'Node Js' },
+            { title: 'Experss Js' },
+            { title: 'Styled Components' },
+        ]
+
+    },
+    {
+        role: [
+            { title: 'Writing reusable, testable, and efficient code.' },
+            { title: 'Take an active part in architecture decisions and complex development tasks.' },
+            { title: 'Design and implement low-latency, high-availability, and performant applications.' },
+            { title: 'Motivated to learn new technologies and bring them to life through proper implementations.' },
+            { title: 'Work and communicate in a team environment.' },
+            { title: 'Like to take on responsibilities and think proactively.' },
+        ],
+        icon: reactJsIcon,
+        title: 'React Developer',
+        companyName: 'Programming Dev',
+        employe: 12345,
+        experience: '2-5 Years',
+        jobType: 'Part Time',
+        salary: '45k-50k',
+        about: 'We are looking for a Node.js Developer responsible for managing the interchange of data between the server and the users. Your primary focus will be the development of all server-side logic, definition and maintenance of the central database, and ensuring high performance and responsiveness to requests from the front-end. You will also be responsible for integrating the front-end elements built by your co-workers into the application. Therefore, a basic understanding of front-end technologies is necessary as well. ',
+        skills: [
+            { title: 'JavaScript' },
+            { title: 'React Js' },
+            { title: 'Node Js' },
+            { title: 'Experss Js' },
+            { title: 'Styled Components' },
+        ]
+
+    },
+    {
+        role: [
+            { title: 'Writing reusable, testable, and efficient code.' },
+            { title: 'Take an active part in architecture decisions and complex development tasks.' },
+            { title: 'Design and implement low-latency, high-availability, and performant applications.' },
+            { title: 'Motivated to learn new technologies and bring them to life through proper implementations.' },
+            { title: 'Work and communicate in a team environment.' },
+            { title: 'Like to take on responsibilities and think proactively.' },
+        ],
+        icon: reactJsIcon,
+        title: 'React Developer',
+        companyName: 'Programming Dev',
+        employe: 12345,
+        experience: '2-5 Years',
+        jobType: 'Part Time',
+        salary: '45k-50k',
+        about: 'We are looking for a Node.js Developer responsible for managing the interchange of data between the server and the users. Your primary focus will be the development of all server-side logic, definition and maintenance of the central database, and ensuring high performance and responsiveness to requests from the front-end. You will also be responsible for integrating the front-end elements built by your co-workers into the application. Therefore, a basic understanding of front-end technologies is necessary as well. ',
+        skills: [
+            { title: 'JavaScript' },
+            { title: 'React Js' },
+            { title: 'Node Js' },
+            { title: 'Experss Js' },
+            { title: 'Styled Components' },
+        ]
+
+    },
+]
+
+export default jobsData;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,45 +3,50 @@ import JobCard from "../../components/JobCard/JobCard";
 import "./Home.css";
 import jobIcon from "../../images/icons/tech-reactjs.svg";
 import JobDetails from "../../components/JobDetails/JobDetails";
+import jobsData from '../../FakeData/JobsData';
 const Home = () => {
-  const [keywords, setKeywords] = useState("");
-  const handleChange = (e) => {
-    const newKeywords = e.target.value;
-    console.log(newKeywords);
-    setKeywords(newKeywords);
-    e.preventDefault();
-  };
-  const handleSubmit = (e) => {
-    console.log(keywords);
-    alert(`We got   : ${keywords}`);
-    e.preventDefault();
-  };
-  return (
-    <section id="home" className="home section">
-      <div className="my-container">
-        <form onSubmit={handleSubmit} className="search_bar">
-          <input
-            placeholder="Search Your Desired Job"
-            onChange={handleChange}
-            type="text"
-          />
-          <button type="submit"> Search </button>
-        </form>
-        <div className="home_container">
-          <div className="card_container">
-            <JobCard icon={jobIcon} />
-            <JobCard />
-            <JobCard />
-            <JobCard />
-            <JobCard />
-          </div>
-          <div className="job_details">
-            <JobDetails />
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+    const [activeCard, setActiveCard] = useState(jobsData?.[0]);
+    const [keywords, setKeywords] = useState("");
+    const handleChange = (e) => {
+        const newKeywords = e.target.value;
+        console.log(newKeywords);
+        setKeywords(newKeywords);
+        e.preventDefault();
+    };
+    const handleSubmit = (e) => {
+        console.log(keywords);
+        alert(`We got   : ${keywords}`);
+        e.preventDefault();
+    };
+    return (
+        <section id="home" className="home section">
+            <div className="my-container">
+                <form onSubmit={handleSubmit} className="search_bar">
+                    <input
+                        placeholder="Search Your Desired Job"
+                        onChange={handleChange}
+                        type="text"
+                    />
+                    <button type="submit"> Search </button>
+                </form>
+                <div className="home_container">
+                    <div className="card_container">
+                        {
+                            jobsData.map(data =>
+                                <JobCard
+                                    data={data}
+                                    activeCard={activeCard}
+                                    setActiveCard={setActiveCard}
+                                />)
+                        }
+                    </div>
+                    <div className="job_details">
+                        <JobDetails data={activeCard} />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Home;
